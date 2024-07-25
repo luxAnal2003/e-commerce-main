59b28d06c0b4429556e9f6e969a11faa9ca01489
luccy <valbarracin2003@gmail.com>
2024-07-25 13:42:16 -0500

    Se añade funcionalidad

--- dashboards/EncargadoInventarios.php
+++ dashboards/EncargadoInventarios.php
@@ -5,12 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard Encargado de Inventarios</title>
         <link rel="stylesheet" href="../css/navbar-footer.css">
-        <link rel="stylesheet" href="../css/style.css">
         <style>
             .contenedor{
                 padding: 20px;
                 text-align: left;
             }
+            
             .contenido {
                 display: none;
             }
@@ -110,123 +110,18 @@
         </style>
     </head>
     <body>
+        <?php
+            if(isset($_SESSION['user_id'])) {
+                $userLoggedIn = true;
+                $userRole = $_SESSION['role'];
+            } else {
+                $userLoggedIn = false;
+            }
+        ?>
         <?php include '../navbars/EncargadoInventarios.html'; ?>
-        <main style="margin-top: 110px;">
-            <div class="contenedor">
-               
-                <section class="productos">
-                    <h2>Todos los productos</h2>
-                    <div class="listaProductos">
-                        <?php
-                        require '../database/connection.php';
-                        $query = "SELECT * FROM Productos ORDER BY nombre ASC";
-                        $result = $conn->query($query);
-
-                        while ($row = $result->fetch_assoc()) {
-                            echo '<div class="itemProducto">';
-                            echo '<div class="verProducto" onclick="redirigir(\'producto.php?id=' . $row['id'] . '\')">';
-                            echo '<img src="../assets/uploads/' . $row['imagen_url'] . '" alt="' . $row['nombre'] . '">';
-                            echo '<p>' . $row['nombre'] . '</p>';
-                            echo '<p>$' . $row['precio'] . '</p>';
-                            echo '</div>';
-                            echo '<button style="border-radius: 50px; margin-right: 4px;" onclick="editarProducto(' . $row['id'] . ')">Editar</button>';
-                            echo '<button style="border-radius: 50px" onclick="eliminarProducto(' . $row['id'] . ')">Eliminar</button>';
-                            echo '</div>';
-                        }
-                        ?>
-                    </div>
-                </section>
-                
-                <div id="clientesPorResponder" class="contenido">
-            <section class="resena">
-                    <div class="resena">
-                        <p>Usuario: Juan Pérez</p>
-                        <p>Momento del envío: 2024-07-01 10:15:00</p>
-                        <p>Mensaje: "Compré estos auriculares la semana pasada y la calidad del sonido es increíble. La cancelación de ruido es excelente y el micrófono funciona a la perfección."</p>
-                        <a href="#" class="replicar">Replicar</a>
-                    </div>
-                    <hr>
-                    <div class="resena">
-                        <p>Usuario: Juan Pérez</p>
-                        <p>Momento del envío: 2024-07-01 10:15:00</p>
-                        <p>Mensaje: "Compré estos auriculares la semana pasada y la calidad del sonido es increíble. La cancelación de ruido es excelente y el micrófono funciona a la perfección."</p>
-                        <a href="#" class="replicar">Replicar</a>
-                    </div>
-                    <hr>
-                    <div class="resena">
-                        <p>Usuario: Juan Pérez</p>
-                        <p>Momento del envío: 2024-07-01 10:15:00</p>
-                        <p>Mensaje: "Compré estos auriculares la semana pasada y la calidad del sonido es increíble. La cancelación de ruido es excelente y el micrófono funciona a la perfección."</p>
-                        <a href="#" class="replicar">Replicar</a>
-                    </div>
-                    <hr>
-                </section>
-            </div>
-            <div id="clientesRespondidos" class="contenido">
-            <section class="resena">
-                    <div class="resena">
-                        <p>Usuario: Juan Pérez</p>
-                        <p>Momento del envío: 2024-07-01 10:15:00</p>
-                        <p>Mensaje: "Compré estos auriculares la semana pasada y la calidad del sonido es increíble. La cancelación de ruido es excelente y el micrófono funciona a la perfección."</p>
-
-                        <div class="replica">
-                            <p>Usuario: Carlos García</p>
-                            <p>Momento del envío: 2024-07-02 12:05:00</p>
-                            <p>Mensaje: "@María López, tuve el mismo problema. Lo solucioné actualizando el software Logitech G HUB. Después de la actualización, el micrófono funcionó mucho mejor."</p>
-
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="resena">
-                        <p>Usuario: Juan Pérez</p>
-                        <p>Momento del envío: 2024-07-01 10:15:00</p>
-                        <p>Mensaje: "Compré estos auriculares la semana pasada y la calidad del sonido es increíble. La cancelación de ruido es excelente y el micrófono funciona a la perfección."</p>
-                        <div class="replica">
-                            <p>Usuario: Carlos García</p>
-                            <p>Momento del envío: 2024-07-02 12:05:00</p>
-                            <p>Mensaje: "@María López, tuve el mismo problema. Lo solucioné actualizando el software Logitech G HUB. Después de la actualización, el micrófono funcionó mucho mejor."</p>
-
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="resena">
-                        <p>Usuario: Juan Pérez</p>
-                        <p>Momento del envío: 2024-07-01 10:15:00</p>
-                        <p>Mensaje: "Compré estos auriculares la semana pasada y la calidad del sonido es increíble. La cancelación de ruido es excelente y el micrófono funciona a la perfección."</p>
-
-                        <div class="replica">
-                            <p>Usuario: Carlos García</p>
-                            <p>Momento del envío: 2024-07-02 12:05:00</p>
-                            <p>Mensaje: "@María López, tuve el mismo problema. Lo solucioné actualizando el software Logitech G HUB. Después de la actualización, el micrófono funcionó mucho mejor."</p>
-
-                        </div>
-                    </div>
-                    <hr>
-                </section>
-            </div>
-
-        </div>
-
-            </div>
+        <main>
+            <?php include '../views/productosAlfab.php'; ?>
         </main>
         <?php include '../templates/footer.html'; ?>
-        <script>
-            function redirigir(url) {
-                window.location.href = url;
-            }
-
-            function editarProducto(id) {
-                // Lógica para editar producto
-                redirigir('editar_producto.php?id=' + id);
-            }
-
-            function eliminarProducto(id) {
-                if (confirm('¿Estás seguro de que deseas eliminar este producto?')) {
-                    // Lógica para eliminar producto
-                    redirigir('eliminar_producto.php?id=' + id);
-                }
-            }
-                   
-        </script>
     </body>
 </html>
